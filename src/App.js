4087c791474da21f0ed0388df3c47f2cfc1891e0
Arun Praba <44196146+arunprabak@users.noreply.github.com>
2021-04-30 18:12:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import "./styles.css";
 
 const types = [
   "video/webm",
+  "video/mp4",
   "audio/webm",
+  "video/mpeg"
   "video/webm;codecs=vp8",
   "video/webm;codecs=daala",
+  "video/webm;codecs=vp9",
+  "video/webm;codecs=vp8.0",
+  "video/webm;codecs=vp9.0",
   "video/webm;codecs=h264",
+  "video/webm;codecs=H264",
+  "video/webm;codecs=avc1",
+  "video/webm;codecs=vp8,opus",
+  "video/WEBM;codecs=VP8,OPUS",
+  "video/webm;codecs=vp9,opus",
+  "video/webm;codecs=vp8,vp9,opus",
+  "video/webm;codecs=h264,opus",
+  "video/webm;codecs=h264,vp9,opus",
+  "video/x-matroska;codecs=avc1",
   "audio/webm;codecs=opus",
-  "video/mpeg"
 ];
 
 export default function App() {
